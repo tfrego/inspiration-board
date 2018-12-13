0ab69297b802380bce304f6288581f70a40298fe
Trang <trang.frego@gmail.com>
2018-12-13 13:42:31 -0800

    update Board with axios get request to API

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,25 +5,36 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
+
+const URL = 'https://inspiration-board.herokuapp.com/boards/Trang/cards';
 
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      cards: CARD_DATA,
+      cards: [],
     };
   }
 
   componentDidMount() {
-    console.log('COMPONENT MOUNTED');
-    console.log(this.state);
+    axios.get(URL)
+      .then((response) => {
+        const cards = response.data.map((card) => {
+          console.log(card.card);
+          const newCard = {
+            ...card.card,
+          }
+          return newCard;
+        })
+        this.setState({cards: cards});
+      })
   }
 
   render() {
-    const cardCollection = this.state.cards.cards.map((card, i) => {
-      return <Card key={i} text={card.text} emoji={card.emoji} />
+    const cardCollection = this.state.cards.map((card, i) => {
+      return <Card key={card.id} text={card.text} emoji={card.emoji} />
     })
     return (
       <div className="board">
