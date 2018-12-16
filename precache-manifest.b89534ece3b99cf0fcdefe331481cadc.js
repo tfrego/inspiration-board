41a74df013f67afe13614de38963734332fba37a
Trang <trang.frego@gmail.com>
2018-12-16 14:00:12 -0800

    Updates

---
+++ precache-manifest.b89534ece3b99cf0fcdefe331481cadc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c56e6dd52f7dbfd5f06",
+    "url": "/inspiration-board/static/css/main.c8031e06.chunk.css"
+  },
+  {
+    "revision": "2c56e6dd52f7dbfd5f06",
+    "url": "/inspiration-board/static/js/main.2c56e6dd.chunk.js"
+  },
+  {
+    "revision": "84a07448a544b4db8a79",
+    "url": "/inspiration-board/static/js/1.84a07448.chunk.js"
+  },
+  {
+    "revision": "d769499afd0cba5bf1c2",
+    "url": "/inspiration-board/static/js/runtime~main.d769499a.js"
+  },
+  {
+    "revision": "16de6770e1f4d6e0d487ec3e611567ce",
+    "url": "/inspiration-board/index.html"
+  }
+];
